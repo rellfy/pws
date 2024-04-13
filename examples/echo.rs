3685a713bffa1be8ca825f167f03dd7a070da1ce
Lorenzo Fontoura <lorenzo@nioel.com>
2024-04-13 17:12:01 +1000

    chore: adjust example

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,5 +1,6 @@
 use pws::{connect_persistent_websocket_async, Message, Url};
 use std::str::FromStr;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() {
@@ -10,11 +11,16 @@ async fn main() {
     tokio::join!(
         async move {
             println!("sending: hello");
-            tx.send(Message::Text("hello".to_owned())).unwrap();
+            tx.send(Message::Text("hello".to_owned())).await.unwrap();
+            tokio::time::sleep(Duration::from_secs(3)).await;
             println!("closing connection");
-            tx.send(Message::Close(None)).unwrap();
+            tx.send(Message::Close(None)).await.unwrap();
+            tokio::time::sleep(Duration::from_secs(3)).await;
             println!("sending: hello again");
-            tx.send(Message::Text("hello again".to_owned())).unwrap();
+            tx.send(Message::Text("hello again".to_owned()))
+                .await
+                .unwrap();
+            tokio::time::sleep(Duration::from_secs(3)).await;
         },
         async move {
             while let Ok(msg) = rx.recv().await {
@@ -22,6 +28,9 @@ async fn main() {
                     continue;
                 };
                 println!("received: {msg}");
+                if &msg == "hello again" {
+                    std::process::exit(0);
+                }
             }
         }
     );
