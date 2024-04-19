b2f2b65322ebc37ef8aa68287eee8face9ef2706
Lorenzo Fontoura <lorenzo@nioel.com>
2024-04-20 03:53:40 +1000

    chore: implement auto close example

---
+++ examples/auto_close.rs
@@ -0,0 +1,31 @@
+use pws::{connect_persistent_websocket_async, Message, Url};
+use std::str::FromStr;
+
+#[tokio::main]
+async fn main() {
+    let url = Url::from_str("wss://echo.websocket.org").unwrap();
+    let (tx, mut rx) = connect_persistent_websocket_async(url).await.unwrap();
+    let mut close_count = 0;
+    while let Ok(msg) = rx.recv().await {
+        match msg {
+            Message::Text(msg) => {
+                println!("received: {msg}");
+                tx.send(Message::Close(None)).await.unwrap();
+            }
+            Message::ConnectionOpened => {
+                println!("connection opened ({close_count})");
+                let msg = format!("hello! connection #{close_count}");
+                tx.send(Message::Text(msg)).await.unwrap();
+            }
+            Message::ConnectionClosed => {
+                println!("connection closed");
+                close_count += 1;
+                if close_count >= 2 {
+                    break;
+                }
+            }
+            _ => {}
+        }
+    }
+    println!("persistent websocket is done");
+}
